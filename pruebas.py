6272fa202c86ddc56b196f1929a6c799397a1780
Miguel Chaparro <machaparrov@correo.udistrital.edu.co>
2022-06-29 17:55:41 -0500

    cambio

---
+++ pruebas.py
@@ -0,0 +1,7 @@
+n=[]
+datosaingresar=int(input("datosaingresar= "))
+while datosaingresar>0:
+        dato=input("ingresa el dato que quieras= ")
+        datosaingresar-=1
+        n.append(dato)
+print("Lista",n)
